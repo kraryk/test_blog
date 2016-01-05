4de06b8321b3b5791eb59f6abe291cd94857fbe1
krak mike <git@keepsteep.pl>
2016-01-05 16:32:47 +0100

    dodana kolumna picture do posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221104954) do
+ActiveRecord::Schema.define(version: 20160105151704) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151221104954) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   create_table "users", force: true do |t|
