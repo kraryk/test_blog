60c8dc6e20ed8912e4d65087c517eebd3b419187
krak mike <git@keepsteep.pl>
2016-01-08 10:57:43 +0100

    usunięto kolumnę role z tabeli users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105151704) do
+ActiveRecord::Schema.define(version: 20160108094740) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20160105151704) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "role"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
