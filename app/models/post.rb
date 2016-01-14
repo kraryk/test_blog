a18ea0a8acefbb8606c0bf8e2065118a0fccb844
krak mike <git@keepsteep.pl>
2016-01-14 11:10:33 +0100

    BŁąd przy wywoływaniu @post.tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_and_belongs_to_many :tags
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108094740) do
+ActiveRecord::Schema.define(version: 20160113184103) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 20160108094740) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
+  end
+
+  create_table "posts_tags", id: false, force: true do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
